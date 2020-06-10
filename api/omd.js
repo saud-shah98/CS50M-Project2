efa2e351ba5a3b7a27ca73679b2aa19ef5fa365e
MSS <mss@MSSs-MacBook-Pro.local>
2020-06-09 22:37:21 -0400

    remove unnecessary code

--- api/omd.js
+++ api/omd.js
@@ -1,14 +1,1 @@
 
-
-
-const processMovie = movie => ({
-    title: movie.Title,
-    yea: movie.Year
-})
-
-export const getMovies = async (params) => {
-    const API_URL = `'http://www.omdbapi.com/?t=${params}&apikey=d7ec2628'`
-    const response = await fetch(API_URL);
-    const {results} = await response.json();
-    return results.map(processMovie)
-}
